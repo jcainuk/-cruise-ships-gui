4046bb49c16e567f2985d5d062d08ee30a770ee4
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-23 16:26:52 +0000

    Changed Port.js into IIFE function

--- src/Port.js
+++ src/Port.js
@@ -1,13 +1,21 @@
-function Port(name) {
-  this.name = name;
-  this.ships = [];
-}
+(function exportPort() {
+  function Port(name) {
+    this.name = name;
+    this.ships = [];
+  }
 
-Port.prototype.addShip = function (ship) {
-  this.ships.push(ship);
-};
+  Port.prototype = {
+    addShip(ship) {
+      this.ships.push(ship);
+    },
+    removeShip(ship) {
+      this.ships = this.ships.filter(dockedShip => dockedShip !== ship);
+    },
+  };
 
-Port.prototype.removeShip = function (ship) {
-  this.ships.pop(ship);
-};
-module.exports = Port;
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Port;
+  } else {
+    window.Port = Port;
+  }
+}());
