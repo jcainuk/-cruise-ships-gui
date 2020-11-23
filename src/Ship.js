913476d5093ac1c260f1edd82e174df6ab6b0304
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-23 16:13:40 +0000

    Added code to src/js files

---
+++ src/Ship.js
@@ -0,0 +1,29 @@
+function Ship(itinerary) {
+  this.itinerary = itinerary;
+  this.currentPort = itinerary.ports[0];
+  this.previousPort = null;
+  this.currentPort.addShip(this);
+}
+
+Ship.prototype.setSail = function () {
+  const { itinerary } = this;
+  const currentPortIndex = itinerary.ports.indexOf(this.currentPort);
+
+  if (currentPortIndex === (itinerary.ports.length - 1)) {
+    throw new Error('End of itinerary reached');
+  }
+
+  this.previousPort = this.currentPort;
+  this.currentPort = null;
+  this.previousPort.removeShip(this);
+};
+
+Ship.prototype.dock = function () {
+  const { itinerary } = this;
+  const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
+
+  this.currentPort = itinerary.ports[previousPortIndex + 1];
+  this.currentPort.addShip(this);
+};
+
+module.exports = Ship;
