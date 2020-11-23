fc0216c12a150b6cfc70b64fc2ef6084020021f9
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-23 15:16:13 +0000

    Created an array of the background images in initialiseSea method

--- src/Controller.js
+++ src/Controller.js
@@ -3,5 +3,9 @@ function Controller() {
 }
 
 Controller.prototype.initialiseSea = function initialiseSea() {
+  const backgrounds = [
+    './images/water0.png',
+    './images/water1.png',
+  ];
   window.setInterval(() => { }, 1000);
 };
